11beeefa03aafa3fdfb517632a44cb1a81dbed90
David Cruwys <david@ideasmen.com.au>
2022-02-04 21:16:15 +1100

    chore: github actions (ci/cd)

--- .builders/generators/01-bootstrap.rb
+++ .builders/generators/01-bootstrap.rb
@@ -66,21 +66,21 @@ KManager.action :bootstrap do
 
         dd = OpenStruct.new(dom)
 
-        oadd("lib/#{dd.application}.rb"             , template_file: 'lib/applet_name.rb'         , dom: dom)
-        oadd("lib/#{dd.application}/version.rb"     , template_file: 'lib/applet_name/version.rb' , dom: dom)
+        add("lib/#{dd.application}.rb"             , template_file: 'lib/applet_name.rb'         , dom: dom)
+        add("lib/#{dd.application}/version.rb"     , template_file: 'lib/applet_name/version.rb' , dom: dom)
     
-        # add('spec/spec_helper.rb')
-        # add("spec/#{dd.application}_spec.rb"       , template_file: 'spec/applet_name_spec.rb', dom: dom)
+        add('spec/spec_helper.rb')
+        add("spec/#{dd.application}_spec.rb"       , template_file: 'spec/applet_name_spec.rb', dom: dom)
 
-        # add("#{dd.application}.gemspec"            , template_file: 'applet_name.gemspec', dom: dom)
-        # add('Gemfile', dom: dom)
-        # add('Guardfile', dom: dom)
-        # add('Rakefile', dom: dom)
-        # add('.rspec', dom: dom)
-        # add('.rubocop.yml', dom: dom)
-        # add('README.md', dom: dom)
-        # add('docs/CODE_OF_CONDUCT.md', dom: dom)
-        # add('docs/LICENSE.txt', dom: dom)
+        add("#{dd.application}.gemspec"            , template_file: 'applet_name.gemspec', dom: dom)
+        add('Gemfile', dom: dom)
+        add('Guardfile', dom: dom)
+        add('Rakefile', dom: dom)
+        add('.rspec', dom: dom)
+        add('.rubocop.yml', dom: dom)
+        add('README.md', dom: dom)
+        add('docs/CODE_OF_CONDUCT.md', dom: dom)
+        add('docs/LICENSE.txt', dom: dom)
 
         # run_command("rubocop -a")
       
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
-require "k_config"
+require 'pry'
+require 'bundler/setup'
+require 'k_config'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
+  config.filter_run_when_matching :focus
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
